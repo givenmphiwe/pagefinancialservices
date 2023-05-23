bd1597db4d15e537b0c8e76e5652e34459c24b38
givenmphiwe <giggslive52@gmail.com>
2023-05-23 10:30:56 +0200

    Starting Page web app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  useRoutes,
+} from "react-router-dom";
+import { Home } from "./components/Home";
 
-function App() {
+import ScrollToTop from "./ScrollToTop";
+
+
+const App = () => {
+  let routes = useRoutes([
+    { path: "/", element: <Home /> },
+    
+  ]);
+  return routes;
+};
+
+const AppWrapper = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <ScrollToTop />
+      <App />
+    </Router>
   );
-}
+};
 
-export default App;
+export default AppWrapper;
+
+// i think the part of complete the part of the complete and do more is great and is noo more greater tha this..
