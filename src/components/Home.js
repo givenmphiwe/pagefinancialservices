e1858a0a60820d19a8cd82d79b8e9baac873fef2
givenmphiwe <giggslive52@gmail.com>
2023-05-23 15:11:43 +0200

    am winning

--- src/components/Home.js
+++ src/components/Home.js
@@ -16,10 +16,22 @@ export const Home = () => {
         <h2 className="top-left-2">With our powerful tools</h2>
         <p className="top-left-3">
           Page financial services is designed in a way that grows your business
-          in all aspects <br/>
+          in all aspects <br />
         </p>
 
         <button className="btn-in-touch">Get in touch</button>
+      </div>
+
+      <div className="container-1">
+        <h3>Who we are and what we do</h3>
+        <p>
+          Page financial services is a leading tech driven sufficireneojoiejcewd fneof fwooief fewfikewnmf
+          eofjeeowi weifjowc coew
+          ewfejweej9efwf
+          wefwefewf oewjfioew jewkfnef jefnwkkjfnew efnewfufehu wejfdbkjewjfb jefkbewb efjk
+           fewfikrewnmf wefei ewfiewufh fewfhie fjewfbueifiwfvewoc edjbcewiu efubuewfge
+          ewfrrr
+        </p>
       </div>
     </>
   );
