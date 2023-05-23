71bb007ab54b91a3968062eeaf34b5a865b2fd2a
givenmphiwe <giggslive52@gmail.com>
2023-05-23 13:34:22 +0200

    today aam fucken cold

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,18 @@
-import NavbarPage from "./Navbar"
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./styles/home.css";
+import LandingImg from "../assets/LandingImg.jpg";
+import Navbar from "./Navbar";
 
 export const Home = () => {
-    return (
-        <>
-        <NavbarPage />
-        <div>Hello page</div>
-        </>
-    )
-}
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <Navbar/>
+      <div>
+        <img  className="Landing-img" src={LandingImg}/>
+      </div>
+    </>
+  );
+};
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,41 @@
-import { useRef } from "react";
-import { FaBars, FaTimes } from "react-icons/fa";
-import { useNavigate } from "react-router-dom";
-import "../components/styles/navbar.css";
+import React, { useRef } from "react";
+import { FaBars, FaTimes, FaReact } from "react-icons/fa";
+import "./styles/navbar.css";
 
-const NavbarPage = () => {
-  const navigate = useNavigate();
+function Navbar() {
+  /* creates a reference to the nav element in the component. 
+    useRef hook is used to create a reference to the element so that it can be accessed and manipulated in the component.*/
 
   const navRef = useRef();
 
+  /*Below line defines the showNavbar function, which toggles the "responsive" class on the nav element. 
+    The classList.toggle method is used to add or remove a class from the element.*/
+
   const showNavbar = () => {
-    navRef.current.classList.remove("responsive_nav")
+    navRef.current.classList.toggle("responsive");
   };
 
   return (
-    <></>
+    <div className="container">
+      {/* Renders a react icon with the size of 40px */}
+      <FaReact size={40} />
+      {/* Sets the navRef as a reference to the nav element */}
+      <nav ref={navRef}>
+        <a href="/#">Home</a>
+        <a href="/#">Blog</a>
+        <a href="/#">About</a>
+        <a href="/#">Contact</a>
+        {/* Renders a button with the class of nav-btn nav-close-btn and an FaTimes icon inside */}
+        <button className="nav-btn nav-close-btn" onClick={showNavbar}>
+          <FaTimes />
+        </button>
+      </nav>
+      {/* Renders a button with the class of nav-btn and an FaBars icon inside */}
+      <button className="nav-btn" onClick={showNavbar}>
+        <FaBars />
+      </button>
+    </div>
   );
-};
+}
 
-export default NavbarPage;
+export default Navbar;
